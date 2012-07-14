fc6aa5e66f376bac876fe09ca51f8151492f01bf
SeanJA <sean.sandy@gmail.com>
2012-07-13 23:52:24 -0300

    camelCase functions and vars instead of underscore_functions and vars
    nodejs is a camelcase language... doh

--- melltjs/lib/mellt.js
+++ melltjs/lib/mellt.js
@@ -1,13 +1,13 @@
 /**
  * Check a password
  */ 
-exports.check_password = function(password) {
+exports.checkPassword = function(password) {
 	//make sure it is lower case, this function can be called by itself
 	password = password.toLowerCase();
-	if (this.check_common(password)) {
+	if (this.checkCommon(password)) {
 		return -1;
 	}
-	days = this.brute_force_days(password);
+	days = bruteForceDays(password);
 	return days;
 }
 
@@ -16,13 +16,13 @@ exports.check_password = function(password) {
  * We do this because "password" takes 129 seconds, but is the first
  * thing an attacker will try.
  */
-exports.check_common = function(password) {
+exports.checkCommon = function(password) {
 	//make sure it is lower case, this function can be called by itself
 	password = password.toLowerCase();
-	var common_passwords = require('./common-passwords.json');
+	var commonPasswords = require('./common-passwords.json');
 	var text = password.toLowerCase();
-	for (var t = 0; t < common_passwords.length; t++) {
-		if (common_passwords[t] == text) {
+	for (var t = 0; t < commonPasswords.length; t++) {
+		if (commonPasswords[t] == text) {
 			// If their password exists in the common file, then it's 
 			// zero time to crack this terrible password.
 			return true;
@@ -34,11 +34,11 @@ exports.check_common = function(password) {
 /**
  * Figure out how long it will take to brute force a password
  */
-exports.brute_force_days = function(password) {
+bruteForceDays = function(password) {
 	//make sure it is lower case, this function can be called by itself
 	password = password.toLowerCase();
-	base = get_charset(password);
-	var hashes_per_second = 1000000000;
+	base = getCharset(password);
+	var hashesPerSecond = 1000000000;
 	// Starting at the first character, figure out it's position in the character set
 	// and how many attempts will take to get there. For example, say your password
 	// was an integer (a bank card PIN number for example):
@@ -53,10 +53,10 @@ exports.brute_force_days = function(password) {
 	// ignore anything between 6530 and 9999. Using this logic, 'aaa' would be a worse
 	// password than 'zzz', because the attacker would encounter 'aaa' first. 
 	var attempts = 0;
-	var characters_in_base = base.length;
-	var characters_in_password = password.length;
+	var charactersInBase = base.length;
+	var charactersInPassword = password.length;
 	var position = 0;
-	for (position; position < characters_in_password; position++) {
+	for (position; position < charactersInPassword; position++) {
 		// We power up to the reverse position in the string. For example, if we're trying 
 		// to hack the 4 character PING code in the example above:
 		// First number * (number of characters possible in the charset ^ length of password)
@@ -68,14 +68,14 @@ exports.brute_force_days = function(password) {
 		// and add on the last number
 		// 9
 		// Totals: 6000 + 500 + 20 + 9 = 6529 attempts before we encounter the correct password.
-		var powerOf = characters_in_password - position - 1;
+		var powerOf = charactersInPassword - position - 1;
 		// Character position within the base set. We add one on because strpos is base 
 		// 0, we want base 1.
-		var character_at_position = base.indexOf(password[position]) + 1;
+		var charactersAtPosition = base.indexOf(password[position]) + 1;
 		// If we're at the last character, simply add it's position in the character set
 		// this would be the "9" in the pin code example above.
 		if (powerOf == 0) {
-			attempts = attempts + character_at_position;
+			attempts = attempts + charactersAtPosition;
 		}
 		// Otherwise we need to iterate through all the other characters positions to 
 		// get here. For example, to find the 5 in 25 we can't just guess 2 and then 5
@@ -87,20 +87,20 @@ exports.brute_force_days = function(password) {
 			// character set, X times, where X is our position -1. Then we need to multiply 
 			// that by this character's position.
 			// Multiplier is the (10^4) or (10^3), etc in the pin code example above.
-			var multiplier = Math.pow(characters_in_base, powerOf);
+			var multiplier = Math.pow(charactersInBase, powerOf);
 			// New attempts is the number of attempts we're adding for this position.
-			var new_attempts = character_at_position * multiplier;
+			var newAttempts = charactersAtPosition * multiplier;
 			// Add that on to our existing number of attempts.
-			attempts = attempts + new_attempts;
+			attempts = attempts + newAttempts;
 		}
 	}
 	// We can (worst case) try a billion passwords a second. Calculate how many days it
 	// will take us to get to the password.
-	var per_day = hashes_per_second * 60 * 60 * 24;
+	var perDay = hashesPerSecond * 60 * 60 * 24;
 	// This allows us to calculate a number of days to crack. We use days because anything
 	// that can be cracked in less than a day is basically useless, so there's no point in
 	// having a smaller granularity (hours for example).
-	var days = attempts / per_day;
+	var days = attempts / perDay;
 
 	// If it's going to take more than a billion days to crack, just return a billion. This
 	// helps when code outside this function isn't using bcmath. Besides, if the password 
@@ -118,28 +118,28 @@ exports.brute_force_days = function(password) {
 /**
  * Figure out which character set the password is using (based on the most  "complex" character in it).
  */ 
-get_charset = function(password) {
-	var character_sets = require('./charsets.json');
+getCharset = function(password) {
+	var characterSets = require('./charsets.json');
 	// Figure out which character set the password is using (based on the most 
 	// "complex" character in it).
 	var base = false;
 	var baseKey = null;
 	for (var t = 0; t < password.length; t++) {
 		var char = password[t];
-		var found_character = false;
-		for (var character_set_key = 0; character_set_key < character_sets.length; character_set_key++) {
-			var character_set = character_sets[character_set_key];
-			if (baseKey <= character_set_key && character_set.indexOf(char) > -1) {
-				baseKey = character_set_key;
-				base = character_set;
-				found_character = true;
+		var foundCharacter = false;
+		for (var characterSetKey = 0; characterSetKey < characterSets.length; characterSetKey++) {
+			var characterSet = characterSets[characterSetKey];
+			if (baseKey <= characterSetKey && characterSet.indexOf(char) > -1) {
+				baseKey = characterSetKey;
+				base = characterSet;
+				foundCharacter = true;
 				break;
 			}
 		}
 		// If the character we were looking for wasn't anywhere in any of the 
 		// character sets, assign the largest (last) character set as default.
-		if (!found_character) {
-			base = character_sets[character_sets.length - 1];
+		if (!foundCharacter) {
+			base = characterSets[characterSets.length - 1];
 			break;
 		}
 	}
